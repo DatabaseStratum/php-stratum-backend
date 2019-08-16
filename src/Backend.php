32821c86f19844311ca573057e57d990fed1ebd0
Paul Water <p.r.water@setbased.nl>
2019-08-16 18:34:17 +0200

    Added interface Config.

--- src/Backend.php
+++ src/Backend.php
@@ -12,34 +12,35 @@ interface Backend
   /**
    * Creates the object that does the actual execution of the constant command for the backend.
    *
-   * @param array        $settings The settings from the configuration file.
+   * @param Config       $settings The settings from the PhpStratum configuration file.
    * @param StratumStyle $io       The output decorator.
    *
    * @return ConstantWorker
    */
-  public function createConstantWorker(array $settings, StratumStyle $io): ConstantWorker;
+  public function createConstantWorker(Config $settings, StratumStyle $io): ConstantWorker;
 
   //--------------------------------------------------------------------------------------------------------------------
   /**
    * Creates the object that does the actual execution of the routine loader command for the backend.
    *
-   * @param array        $settings The settings from the configuration file.
+   * @param Config       $settings The settings from the PhpStratum configuration file.
    * @param StratumStyle $io       The output decorator.
    *
    * @return RoutineLoaderWorker
    */
-  public function createRoutineLoaderWorker(array $settings, StratumStyle $io): RoutineLoaderWorker;
+  public function createRoutineLoaderWorker(Config $settings, StratumStyle $io): RoutineLoaderWorker;
 
   //--------------------------------------------------------------------------------------------------------------------
   /**
    * Creates the object that does the actual execution of the routine wrapper generator command for the backend.
    *
-   * @param array        $settings The settings from the configuration file.
+   * @param Config       $settings The settings from the PhpStratum configuration file.
    * @param StratumStyle $io       The output decorator.
    *
    * @return RoutineWrapperGeneratorWorker
    */
-  public function createRoutineWrapperGeneratorWorker(array $settings, StratumStyle $io): RoutineWrapperGeneratorWorker;
+  public function createRoutineWrapperGeneratorWorker(Config $settings,
+                                                      StratumStyle $io): RoutineWrapperGeneratorWorker;
 
   //--------------------------------------------------------------------------------------------------------------------
 }
