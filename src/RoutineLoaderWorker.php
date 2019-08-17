6b7e28f88f5f303b1f2c3570d4e006971a232631
Paul Water <p.r.water@setbased.nl>
2019-08-17 11:39:29 +0200

    Added $sources argument to RoutineLoaderWorker::execute().

--- src/RoutineLoaderWorker.php
+++ src/RoutineLoaderWorker.php
@@ -13,9 +13,12 @@ interface RoutineLoaderWorker
    * Does the actual execution of the routine loader command for the backend. Returns 0 on success. Otherwise returns
    * nonzero.
    *
+   * @param array|null $sources An optional list of paths to sources of stored routines that must be loaded. If null all
+   *                            sources must be loaded.
+   *
    * @return int
    */
-  public function execute(): int;
+  public function execute(?array $sources): int;
 
   //--------------------------------------------------------------------------------------------------------------------
 }
